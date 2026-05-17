714eb3e9752458efc67f7998d64673ba05ae669c
aldangarcia <601540@students.esic.university>
2026-05-17 19:13:48 +0200

    add visualizations for results

--- src/comparison.py
+++ src/comparison.py
@@ -2,7 +2,8 @@ import pandas as pd
 
 fp16_df = pd.read_csv("./results/phi2_fp16_baseline.csv")
 int8_df = pd.read_csv("./results/phi2_int8.csv")
+int4_df = pd.read_csv("./results/phi2_int4.csv")
 
-comparison = pd.concat([fp16_df, int8_df])
+comparison = pd.concat([fp16_df, int8_df, int4_df])
 
 print(comparison)
